ba48eabe59ebf06aa4074d93fca06cceeda70416
Curtis F Kaylor <revcurtisp@gmail.com>
2023-08-14 18:55:12 -0400

    Initial commit after copy from repository Aquarius/s3basic

---
+++ s3basic/fcrom.bat
@@ -0,0 +1,2 @@
+@REM Display differences between S3 BASIC and S2 BASIC binaries
+fc /b ..\disassembly\aquarius-s2.rom s3basic.cim 
---
+++ s3basic/rom2aql.bat
@@ -0,0 +1,5 @@
+@REM Move Assembled ROM to AquaLite ROMs Directory
+@REM Requires environment variable "AquaLite" SET to 
+@REM   AquaLite base directory
+copy s3basic.cim %AquaLite%\roms\aqubasic.rom
+
