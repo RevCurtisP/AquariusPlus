12bdb2325bb57e2f2627c2b8a586568348aa1bb7
Curtis F Kaylor <revcurtisp@gmail.com>
2023-09-02 10:34:32 -0400

    Fixed bug: paged LOAD would overflow on exactly 16384 bytes

--- plusBasic/aqplusbas.asm
+++ plusBasic/aqplusbas.asm
@@ -223,7 +223,7 @@ _coldboot:
 .print_basic
     call    print_string_immd
 .plus_text
-    db "plusBASIC v0x10b", 0
+    db "plusBASIC v0x10c", 0
 .plus_len   equ   $ - .plus_text
 
     call    CRDO
