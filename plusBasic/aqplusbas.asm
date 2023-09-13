8a92616b2b9084af81e91a4b79f47f05cbf0f01f
Curtis F Kaylor <revcurtisp@gmail.com>
2023-09-12 20:52:21 -0400

    Fixed bug in SET SPRITE TILE

--- plusBasic/aqplusbas.asm
+++ plusBasic/aqplusbas.asm
@@ -212,7 +212,7 @@ _coldboot:
 .print_basic
     call    print_string_immd
 .plus_text
-    db "plusBASIC v0.12", 0
+    db "plusBASIC v0.12a", 0
 .plus_len   equ   $ - .plus_text
 
     call    CRDO
