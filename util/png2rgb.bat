980177d9bddacdb5ef5b5b63cf9d3a55a904e947
Curtis F Kaylor <revcurtisp@gmail.com>
2023-09-24 11:42:27 -0400

    Completed RGB to Aq+ files, added PNG to RGB batch file

---
+++ util/png2rgb.bat
@@ -0,0 +1,1 @@
+magick convert %1.png -crop 8x8 -append %1.rgb
--- util/rgb2tilemap.py
+++ util/rgb2tilemap.py
@@ -4,12 +4,9 @@
 #
 # Converts raw 8-bit RGB file to Aquarius+ Tiles and Tile Map
 #
-# ImageMagick command line to create the rgb file
-# magick convert image.png -crop 8x8 -append tiled.rgb
-#
-# magick convert level1.png -crop 8x8 -append level1.rgb
-# python rgb2tilemap.py level1
-#
+# First, convert PNG to RGB ImageMagick command line to create the rgb file
+#   'png2rgb image' (without the .png extension)
+
 
 import argparse
 import struct 
@@ -18,7 +15,7 @@ import sys
 parser = argparse.ArgumentParser(
     description="Convert Aquarius BASIC .BAS file to text file"
 )
-parser.add_argument("input", help="Input file")
+parser.add_argument("input", help="RGB Input file (without .rgb extension")
 
 debug = False
 
@@ -45,6 +42,8 @@ tile_index = 128
 tile_dict = {}
 
 tile_map = []
+tile_count = {}
+tile_cell = {}
 
 cell_no = 0
 
@@ -81,7 +80,7 @@ with open(rgb_name, "rb") as rgb_file:
       tile = tile + til_line
       tilev = til_line + tilev
       tileh = tileh + tfh_line
-      tilevh = tileh + tilevh 
+      tilevh = tfh_line + tilevh 
     #if debug: print("tile:",tile)
     if tile in tile_dict:
       tm_cell = tile_dict[tile] 
@@ -94,25 +93,19 @@ with open(rgb_name, "rb") as rgb_file:
     else:
       tile_dict[tile] = tile_index
       tm_cell = tile_index
+      tile_count[tile_index] = 0
+      tile_cell[tile_index] = cell_no
       tile_index += 1
+    tile_count[tile_index-1] += 1
     if debug: print("cell#:",cell_no)
     if debug: print("tm_cell:", hex(tm_cell | tm_pal))
     tile_map.append((tm_cell | tm_pal))  
     cell_no += 1
 
-# Open descriptive text file
-txt_file = open(txt_name, "w")
-
 # Generate Aquarius+ tilemap file
 with open(map_name, "wb") as map_file:
-  txt_div = "\n"+"+---" * 64 + "+\n"
-  for cell_no in range(0, len(tile_map)):
-    cell = tile_map[cell_no]
-    if (cell_no & 63) == 0: 
-      txt_file.write(txt_div + "|")
-    txt_file.write("{:>3}|".format(cell & 511))
+  for cell in tile_map:
     map_file.write(cell.to_bytes(2,"little"))
-  txt_file.write(txt_div)
 
 # Generate Aquarius+ palette file
 with open(pal_name, "wb") as pal_file:
@@ -123,4 +116,34 @@ with open(pal_name, "wb") as pal_file:
 with open(til_name, "wb") as til_file:
   for key in tile_dict:
     til_file.write(key)
- 
+
+# Open descriptive text file
+with open(txt_name, "w") as txt_file:
+  # Write text tilemap
+  text_div = "+---" * 64 + "+"
+  for cell_no in range(0, len(tile_map)):
+    cell = tile_map[cell_no]
+    if (cell_no & 63) == 0: 
+      if cell_no:
+        txt_file.write(text_div + "\n")
+        txt_file.write(num_line + "\n")
+        txt_file.write(flp_line + "\n")
+      num_line = "|"
+      flp_line = "|"
+    num_line += "{:>3}|".format(cell & 511)
+    flp_horz = "H" if cell & tm_fliph else " "
+    flp_vert = "V" if cell & tm_flipv else " "
+    flp_line += flp_horz + " " + flp_vert + "|"
+  txt_file.write(text_div + "\n")
+  txt_file.write(num_line + "\n")
+  txt_file.write(flp_line + "\n")
+  txt_file.write(text_div + "\n\n")
+  # Write tile usage counts
+  txt_file.write("Unique tiles: %d\n\n" % len(tile_dict))
+  txt_file.write("Tile# Count Position\n")
+  for index, count in tile_count.items():
+    txt_file.write(" " + str(index) + "{:>7}".format(count))
+    if count == 1: 
+      cell = tile_cell[index]
+      txt_file.write(" (%d,%d)" % (cell & 63, cell / 64))
+    txt_file.write("\n")
