4b8aea5c60dbbaa08434d52bf89bf9ce0ef5940a
Curtis F Kaylor <revcurtisp@gmail.com>
2024-05-13 19:53:12 -0400

    Added utility getver.bas

--- plusBasic/files2www.bat
+++ plusBasic/files2www.bat
@@ -1,9 +1,10 @@
 @REM Copy assembled ROM and documentation to local www folder
 @SET _dest=..\www\release
 copy zout\sysrom.bin %_dest%
-copy README.html %_dest%\quickref.html
+copy README.html %_dest%\quickref.ge
 copy releases.html %_dest%\releases.html
 copy README.md %_dest%\quickref.md
 copy ..\util\bas2baq.py %_dest%
 copy ..\util\baq2bas.py %_dest%
+copy ..\pbutil\getver.bas %_dest%
 copy ..\pbutil\myip.bas %_dest%
