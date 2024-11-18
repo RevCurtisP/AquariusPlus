59dd69eefa16f894d178e64bdb77f7805bb25e1e
Curtis F Kaylor <revcurtisp@gmail.com>
2024-11-17 21:04:25 -0500

    Modified boot.bin to load and execute BASIC ROMs when called from BASIC, changed warm boot behavior to start up already loaded BASIC/cartridge.

--- plusBasic/boot/build.bat
+++ plusBasic/boot/build.bat
@@ -1,2 +1,6 @@
-zmac --zmac -o boot.cim -o boot.lst boot.asm
+@ECHO OFF
+SET _opts=
+if "%1" NEQ "" SET _opts=-D%1
+ECHO ON
+zmac --zmac %_opts% -o boot.cim -o boot.lst boot.asm
 copy boot.cim boot.bin
